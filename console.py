d0ac1839af36e4331cbe9696eeb65d10adb8f814
devmarrie <annemarrieakeyo42@gmail.com>
2022-10-31 07:36:12 +0300

    [feat]:Updating the new classes

--- console.py
+++ console.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-
-from ast import arg
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 """
@@ -15,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     Including a prompt
     """
     prompt = "(hbnb)"
-    model = ["BaseModel","User"]
+    model = ["BaseModel", "User", "Place", "State", "City", "Amenity", "Review"]
     
     def emptyline(self):
         """If not overriden it repeats the last empty line"""
