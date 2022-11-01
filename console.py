280fa55641707c4119bd267aadf34fa007ccff4a
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-01 07:50:18 +0300

    [fix]:console doc

--- console.py
+++ console.py
@@ -21,9 +21,9 @@ class HBNBCommand(cmd.Cmd):
     Including a prompt
     """
     prompt = "(hbnb)"
-    doc_header = 'doc_header'
-    misc_header = 'misc_header'
-    undoc_header = 'undoc_header'
+    doc_header = 'Console_doc_header'
+    misc_header = 'Console_misc_header'
+    undoc_header = 'Console_undoc_header'
     ruler = '-'
     model = ["BaseModel", "User", "Place",
              "State", "City", "Amenity", "Review"
